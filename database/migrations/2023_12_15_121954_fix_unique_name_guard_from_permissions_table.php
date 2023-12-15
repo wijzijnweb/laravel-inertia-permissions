0d37ffbbe799ea14158cacd63901dd204c5da226
tim <tim@doitonlinemedia.nl>
2023-12-15 12:22:17 +0100

    Fix unique name index

---
+++ database/migrations/2023_12_15_121954_fix_unique_name_guard_from_permissions_table.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('permissions', function (Blueprint $table) {
+            $table->dropUnique(['name', 'guard_name']);
+            $table->unique(['name', 'guard_name']);
+        });
+    }
+};
